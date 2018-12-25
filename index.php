489575d0e4790e8d02c5aefd64cfe37893c7c0e4
Dmitrii <dmtokfl@gmail.com>
2018-12-25 18:53:45 +0300

    form

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+if (!empty($_POST)) {
+
+	$message = "Вам пришло новое сообщение: \n"
+	. "Имя пользователя: " . $_POST['userName'] . "\n"
+	. "Email пользователя: " . $_POST['userEmail'] . "\n"
+	. "Сообщение: \n " . $_POST['message'];
+
+	$header = "From: dmtokfl@gmail.com";
+
+
+	$resultMail = mail("dmtr3243@gmail.com", "Сообщение с сайта", $message, $header);
+
+	if ($resultMail) {
+		echo "Сообщение отправлено успешно!";
+	} else {
+		echo "Что-то пошло не так!";
+	}
+}
+?>
+
+<form action="index.php" method="post">
+	<input type="text" name="userName" placeholder="Ваше имя"><br>
+	<input type="text" name="userEmail" placeholder="Ваш Email"><br>
+	<textarea name="message" id="" cols="30" rows="10" placeholder="Сообщение"></textarea><br>
+	<input type="submit" value="Отправить форму!">
+</form>
